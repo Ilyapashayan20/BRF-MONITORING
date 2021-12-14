ce7326694447855790bc2360fb6e1b4cdf4e8ccb
Ilyapashayan20 <77303552+poxka2004@users.noreply.github.com>
2021-12-15 01:00:02 +0400

    add new pages

--- src/javascrip/partial.js
+++ src/javascrip/partial.js
@@ -26,3 +26,44 @@ closemap.addEventListener("click",()=>{
     partial.style.display="block"
 
 })
+
+const partialStartBtn = document.querySelector(".partial-start-btn")
+const paratelbtnclickdisplaynone = document.querySelector(".paratel-btn-click-display-none")
+
+
+
+
+const partialmotivationheader = document.querySelector(".partial-motivation-header")
+
+
+partialStartBtn.addEventListener("click",()=>{
+    paratelbtnclickdisplaynone.style.display = "none"
+    partialStartBtn.style.display = "none"
+    
+})
+
+const vectors = [
+
+ document.querySelector(".vector-6"),
+ document.querySelector(".arrow-2"),
+ document.querySelector(".arrow-3"),
+]
+
+for (let index = 0; index < vectors.length; index++) {
+    const d = vectors[index];
+    d.addEventListener("click",()=>{
+        partialmotivationheader.style.display ="block"
+        partial.style.display = "none"
+    
+    })
+    
+}
+
+
+const vectorclose =  document.querySelector(".vector-7")
+vectorclose.addEventListener("click",()=>{
+    partialmotivationheader.style.display ="none"
+    partial.style.display = "block"
+    console.log("close");
+
+})
